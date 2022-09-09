44e13978750a2d9d176be525374e03d7dd52b90b
David Demerchyan <davidemer11@yandex.ru>
2022-09-09 11:51:22 +0300

    user models created

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        blank=False,
+        verbose_name='Адрес электронной почты'
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=False,
+        verbose_name='Уникальное имя пользователя'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+        verbose_name_plural='Подписчики'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор',
+        verbose_name_plural='Авторы'
+    )
