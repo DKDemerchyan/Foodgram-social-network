3d69556cb04f876ba88a57ce6160bab8c9fd243c
David Demerchyan <davidemer11@yandex.ru>
2022-09-14 15:21:57 +0300

    corrected urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import RecipeViewSet
+from .views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 
 router = DefaultRouter()
 
 router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 
 urlpatterns = [
