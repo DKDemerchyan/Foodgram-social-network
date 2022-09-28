e26fe4ec0c6d1114a9bd5028ffd5f5739fb609ff
David Demerchyan <davidemer11@yandex.ru>
2022-09-28 21:30:55 +0300

    workflow 4

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,11 +1,11 @@
 from djoser.views import UserViewSet
+from recipes.pagination import RecipePagination
 from rest_framework import status
 from rest_framework.generics import ListAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from recipes.pagination import RecipePagination
 from users.models import Follow, User
 from users.serializers import (CustomUserSerializer, FollowListSerializer,
                                FollowSerializer)
