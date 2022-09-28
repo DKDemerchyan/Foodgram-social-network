3c069eff0dbb15a549405ddc378fe2ae83846035
David Demerchyan <davidemer11@yandex.ru>
2022-09-28 21:34:02 +0300

    workflow 5

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -2,7 +2,6 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from recipes.models import Recipe
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
 from users.models import Follow, User
 
 
