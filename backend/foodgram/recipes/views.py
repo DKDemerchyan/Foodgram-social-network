aae79b0418899ddf840274419c6fb8617512bf9d
David Demerchyan <davidemer11@yandex.ru>
2022-09-29 23:45:07 +0300

    noname

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -1,5 +1,5 @@
 #  from django.db.models import Sum
-from django.http import HttpResponse
+#  from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status, viewsets
