d44bb76c15ef5d6d0ec315314d68d80b3ca4e510
David Demerchyan <davidemer11@yandex.ru>
2022-09-30 16:28:55 +0300

    Упростил

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
@@ -108,18 +106,13 @@ class RecipeViewSet(viewsets.ModelViewSet):
             'ingredient__measurement_unit'
         ).annotate(amount=Sum('amount'))
 
-        today = datetime.today()
-        shopping_list = (
-            f'Список покупок для: {user.shopping_carts.recipe}\n\n'
-            f'Дата: {today:%Y-%m-%d}\n\n'
-        )
+        shopping_list = ('Список покупок')
         shopping_list += '\n'.join([
             f'- {ingredient["ingredient__name"]} '
             f'({ingredient["ingredient__measurement_unit"]})'
             f' - {ingredient["amount"]}'
             for ingredient in ingredients
         ])
-        shopping_list += f'\n\nFoodgram ({today:%Y})'
 
         filename = f'{user.username}_shopping_list.txt'
         response = HttpResponse(shopping_list, content_type='text/plain')
