430f12d85acb71efd8c53a436191880fe94b0ca9
David Demerchyan <davidemer11@yandex.ru>
2022-10-01 01:54:48 +0300

    popytka

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -26,7 +26,7 @@ class CustomUserSerializer(UserSerializer):
     def get_is_subscribed(self, obj):
         request = self.context.get('request')
         return (
-            (request and request.user.is_anonymous)
+            (request or not request.user.is_anonymous)
             and Follow.objects.filter(user=request.user, author=obj).exists()
         )
 
